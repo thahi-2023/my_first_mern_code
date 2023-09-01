44e7c29c93c7b5c6f6b3dcd44361d62d5f521f03
unknown <thahira.f2021@gmail.com>
2023-09-01 13:55:57 -0400

    added login and register

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <header>
+    <div>
+
+    </div>
+    </header>
+  )
+}
+
+export default Header
