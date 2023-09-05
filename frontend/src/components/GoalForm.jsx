ccf58492239664577af09a3a102662db3d097b71
unknown <thahira.f2021@gmail.com>
2023-09-05 09:18:04 -0400

    created goal slice

--- frontend/src/components/GoalForm.jsx
+++ frontend/src/components/GoalForm.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from 'react'
-import { useSelector,  useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { createGoal } from '../features/goals/goalSlice'
 
 function GoalForm() {
